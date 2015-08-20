b9f53361aa77474de0c7654e2ea31717f6524b85
Ellis Fantuzzi <ellis.fantuzzi@hotmail.it>
2015-08-20 16:36:37 +0200

    Articoli
    inserimento/modifica/cancellazione di articoli

---
+++ TestCSharp/Controllers/ArticoloController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestCSharp.Models;
+
+namespace TestCSharp.Controllers
+{
+    public class ArticoloController : TestCSharp.WebSite.Base.Controllers.WebsiteContextController
+    {        
+        private Repositories.ArticoloRepository _oArticoloRepo;
+
+        public ArticoloController() : base()
+        {
+            _oArticoloRepo = new Repositories.ArticoloRepository(this.DatabaseFactory);
+        }
+
+        public ActionResult Index()
+        {
+            IQueryable<Articolo> oQuery = _oArticoloRepo.GetAllSimpleList();
+            List<Articolo> oList = oQuery.ToList();
+            return View(oList);
+        }
+
+        public ActionResult Create()
+        {
+            Articolo oArticolo = new Articolo();
+
+            return View(oArticolo);
+        }
+        public ActionResult Insert(Articolo model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                _oArticoloRepo.Add(model);
+                this.DatabaseFactory.GetContext().SaveChanges();
+
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int ID)
+        {
+            Articolo oArticolo = _oArticoloRepo.GetEdit().SingleOrDefault(x => x.ID == ID);
+
+            return View(oArticolo);
+        }
+        public ActionResult Update(Articolo model)
+        {
+            if (ModelState.IsValid)
+            {
+                _oArticoloRepo.Update(model);
+                this.DatabaseFactory.GetContext().SaveChanges();
+
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(Articolo model)
+        {
+            if (model != null && ModelState.IsValid)
+            {
+                // Aggiorno l'operazione
+                Articolo oArticolo = _oArticoloRepo.GetEdit().SingleOrDefault(x => x.ID == model.ID);
+                if (oArticolo != null)
+                    _oArticoloRepo.Delete(oArticolo);
+            }
+
+            this.DatabaseFactory.GetContext().SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TestCSharp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestCSharp.WebSite.Controllers
+{
+    public class HomeController : TestCSharp.WebSite.Base.Controllers.WebsiteContextController
+    {
+        //private Repositories.ArticoloRepository _oArticoloRepo;
+        //private Repositories.MagazzinoRepository _oMagazzinoRepo;
+        //private Repositories.MovimentoRepository _oMovimentoRepo;
+
+        public HomeController() : base()
+        {
+            //_oArticoloRepo = new Repositories.ArticoloRepository(this.DatabaseFactory);
+            //_oMagazzinoRepo = new Repositories.MagazzinoRepository(this.DatabaseFactory);
+            //_oMovimentoRepo = new Repositories.MovimentoRepository(this.DatabaseFactory);
+        }
+
+        public ActionResult Index()
+        {
+            //using (TestCSharp.Models.Entities.TestCSharpContext db = new TestCSharp.Models.Entities.TestCSharpContext())
+            //{
+            //    db.Articoli.ToList();
+            //}
+            return View();
+        }
+
+    }
+}
