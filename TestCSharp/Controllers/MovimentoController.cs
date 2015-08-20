d1cb77984ecb220bfc2737f28af1fb703029c46d
Ellis Fantuzzi <ellis.fantuzzi@hotmail.it>
2015-08-20 17:02:41 +0200

    Magazzini

---
+++ TestCSharp/Controllers/MovimentoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestCSharp.Controllers
+{
+    public class MovimentoController : TestCSharp.WebSite.Base.Controllers.WebsiteContextController
+    {
+        private Repositories.ArticoloRepository _oArticoloRepo;
+        private Repositories.MagazzinoRepository _oMagazzinoRepo;
+        private Repositories.MovimentoRepository _oMovimentoRepo;
+
+        public MovimentoController() : base()
+        {
+            _oMovimentoRepo = new Repositories.MovimentoRepository(this.DatabaseFactory);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
